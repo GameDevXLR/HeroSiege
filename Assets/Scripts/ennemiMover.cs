bc6597e5d9c482f06054956d777e0a7d871c374b
Jeronimoh-PC\Jeronimoh <jeronimoh974.88@gmail.com>
2017-03-01 13:30:52 +0400

    map

---
+++ Assets/Scripts/ennemiMover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ennemiMover : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update()
+	{
+		rb.AddForce (new Vector3 (5, 0.0f, 0.0f)*speed);
+	}
+}
---
+++ Assets/Scripts/ennemiSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ennemiSpawn : MonoBehaviour {
+
+	public GameObject ennemi;
+	public Transform inibTransform;
+	public float waves;
+
+	private GameObject newEnnemi;
+
+	void Start () 
+	{
+		StartCoroutine (spawn ());
+	}
+	
+
+	IEnumerator spawn()
+	{
+		for (float i = 0; i < waves; i++) 
+		{
+			GameObject newEnnemi = Instantiate (ennemi, inibTransform.position, inibTransform.rotation) as GameObject;
+			yield return new WaitForSeconds (5);
+		}
+	}
+}
