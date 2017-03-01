c90d508569cc789ea955c7473ae1f4a3b330c8cd
Marshal974 <mez.guillaume@gmail.com>
2017-03-01 15:59:46 +0400

    stats et fontaine.

---
+++ Assets/Scripts/FountainRegenScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FountainRegenScript : MonoBehaviour {
+
+	// pour que cette fontaine fonctionne : il faut que le script soit mis sur un objet possédant un collider en mode Trigger. Dés qu'un joueur entre dedans il regen.
+
+	public int regenHp = 2;
+	public int regenMp = 1;
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.layer.ToString () == "Player") 
+		{
+			other.GetComponent<GenericLifeScript> ().currentHp += regenHp;
+			other.GetComponent<GenericManaScript> ().currentMp += regenMp;
+		}
+	}
+}
---
+++ Assets/Scripts/GenericManaScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericManaScript : MonoBehaviour {
+
+	//ce script gere la mana de l'objet auquel il est attaché.
+
+	public int maxMp = 100;
+	public int currentMp = 80;
+	public int regenMp;
+	public float timeBetweenTic = 1f;
+	private float lastTic;
+
+	// Use this for initialization
+	void Start () {
+		lastTic = 0f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > lastTic) 
+		{
+			lastTic = Time.time + timeBetweenTic;
+			RegenerateMp ();
+		}
+		if (currentMp > maxMp) 
+		{
+			currentMp = maxMp;
+		}
+		if (currentMp < 0) 
+		{
+			currentMp = 0;
+		}
+	}
+
+	public void RegenerateMp ()
+	{
+		currentMp += regenMp;
+	}
+	public void LooseManaPoints(int mana)
+	{
+		currentMp -= mana;
+	}
+}
