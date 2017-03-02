94b827d498a81ec3205da118a11486bc2743899b
Jeronimoh-PC\Jeronimoh <jeronimoh974.88@gmail.com>
2017-03-02 11:05:34 +0400

    mapJero

---
+++ Assets/Scripts/inibMover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inibMover : MonoBehaviour {
+
+	public float speed;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate( new Vector3 (2, 4, 10) * speed * Time.deltaTime);
+	}
+}
