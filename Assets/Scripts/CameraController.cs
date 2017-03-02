4cd71f92d933327d168fedadeefc8385b866da40
Marshal974 <mez.guillaume@gmail.com>
2017-03-02 14:45:45 +0400

    ajout de la caméra de Exo.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public bool selectedPlayer = true;
+    public int speed = 1;
+    public int zoneDetectionMouse = 100;
+    private Vector3 offset;
+    private float yvalue;
+
+    Camera camera;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+        camera = GetComponent<Camera>();
+        yvalue = gameObject.transform.position.y;
+    }
+
+    void Update()
+    {
+
+   if (Input.mousePosition.x >= 0 && Input.mousePosition.x <= zoneDetectionMouse)
+            {
+                transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+            }
+            else if (Input.mousePosition.x <= Screen.width && Input.mousePosition.x >= Screen.width - zoneDetectionMouse)
+            {
+                transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+            }
+
+            if (Input.mousePosition.y >= 0 && Input.mousePosition.y <= zoneDetectionMouse)
+            {
+                transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
+            }
+            else if (Input.mousePosition.y <= Screen.height && Input.mousePosition.y >= Screen.height - zoneDetectionMouse)
+            {
+                transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+            }
+
+        
+    }
+
+    void LateUpdate()
+    {
+		if (selectedPlayer) {
+			transform.position = player.transform.position + offset;
+		}
+        gameObject.transform.position = new Vector3()
+        {
+            x = gameObject.transform.position.x,
+            y = yvalue,
+            z = gameObject.transform.position.z
+        };
+    }
+
+}
+
+
+
