fb916be44c0b9a459272fbffb91a73f4696cb989
exopole <alexandre.nicaise28@gmail.com>
2017-03-03 14:00:45 +0100

    ajout d'une camera lolLike et possibilité de locker ou non la camera en appuyant sur L

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,51 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    public bool selectedPlayer = true;
+    public bool selectedPlayer = false;
     public int speed = 1;
     public int zoneDetectionMouse = 100;
     private Vector3 offset;
     private float yvalue;
 
-    Camera camera;
+	Camera cameraCible;
 
     void Start()
     {
         offset = transform.position - player.transform.position;
-        camera = GetComponent<Camera>();
+        cameraCible = GetComponent<Camera>();
         yvalue = gameObject.transform.position.y;
     }
 
     void Update()
     {
-
-   if (Input.mousePosition.x >= 0 && Input.mousePosition.x <= zoneDetectionMouse)
-            {
-                transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
-            }
-            else if (Input.mousePosition.x <= Screen.width && Input.mousePosition.x >= Screen.width - zoneDetectionMouse)
-            {
-                transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
-            }
-
-            if (Input.mousePosition.y >= 0 && Input.mousePosition.y <= zoneDetectionMouse)
-            {
-                transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
-            }
-            else if (Input.mousePosition.y <= Screen.height && Input.mousePosition.y >= Screen.height - zoneDetectionMouse)
-            {
-                transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
-            }
-
+		if (Input.GetKey (KeyCode.L))
+			selectedPlayer = !selectedPlayer;
+		if (!Input.GetKey ("space") && !selectedPlayer) {
+			UtilsScreenMovement.moveScreenWithMouse (cameraCible, zoneDetectionMouse, speed);
+//			if (Input.mousePosition.x >= 0 && Input.mousePosition.x <= zoneDetectionMouse) {
+//				transform.Translate (new Vector3 (-speed * Time.deltaTime, 0, 0));
+//			} else if (Input.mousePosition.x <= Screen.width && Input.mousePosition.x >= Screen.width - zoneDetectionMouse) {
+//				transform.Translate (new Vector3 (speed * Time.deltaTime, 0, 0));
+//			}
+//
+//			if (Input.mousePosition.y >= 0 && Input.mousePosition.y <= zoneDetectionMouse) {
+//				transform.Translate (new Vector3 (0, 0, -speed * Time.deltaTime));
+//			} else if (Input.mousePosition.y <= Screen.height && Input.mousePosition.y >= Screen.height - zoneDetectionMouse) {
+//				transform.Translate (new Vector3 (0, 0, speed * Time.deltaTime));
+//			}
+		}
         
     }
 
     void LateUpdate()
     {
-		if (selectedPlayer) {
+		if (selectedPlayer || Input.GetKey ("space")) {
 			transform.position = player.transform.position + offset;
 		}
         gameObject.transform.position = new Vector3()
