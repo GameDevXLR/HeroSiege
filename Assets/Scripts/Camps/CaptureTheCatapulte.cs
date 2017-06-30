6a541de42da139ea8a90d1ecf01d2de972323ee8
exopole <alexandre.nicaise28@gmail.com>
2017-06-30 16:15:08 +0200

    Test de catapulte are. Stop ^particle

--- Assets/Scripts/Camps/CaptureTheCatapulte.cs
+++ Assets/Scripts/Camps/CaptureTheCatapulte.cs
@@ -8,11 +8,17 @@ public class CaptureTheCatapulte : CaptureThePoint
 {
 
     public Animation anime;
+    public ParticleSystem areaParticles;
+    public ParticleSystem cdParticles;
+
+    public bool emissionGo;
 
 
     public override void captureThePoint(PointOwner target)
     {
         base.captureThePoint(target);
+        ParticleSystem.EmissionModule em = cdParticles.emission;
+        em.enabled = true;
         anime.Play();
     }
 
@@ -20,6 +26,8 @@ public class CaptureTheCatapulte : CaptureThePoint
     public override void stopCapture()
     {
         base.stopCapture();
+        ParticleSystem.EmissionModule em = cdParticles.emission;
+        em.enabled = false;
         anime.Stop();
     }
 
