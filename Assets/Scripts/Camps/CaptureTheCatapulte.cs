534064418bf7b8dd0989191ba19f8c5b75a6e94c
exopole <alexandre.nicaise28@gmail.com>
2017-06-30 15:16:57 +0200

    Intégration de l'effet de particules sur la catapulte de gauche

---
+++ Assets/Scripts/Camps/CaptureTheCatapulte.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HyperLuminalGames;
+using UnityEngine.Networking;
+
+public class CaptureTheCatapulte : CaptureThePoint
+{
+
+    public Animation anime;
+
+
+    public override void captureThePoint(PointOwner target)
+    {
+        base.captureThePoint(target);
+        anime.Play();
+    }
+
+
+    public override void stopCapture()
+    {
+        base.stopCapture();
+        anime.Stop();
+    }
+
+}
