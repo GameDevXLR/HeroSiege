6eb2c202f1ed8819ee96866fe6fd44a811e377ff
Jeronimoh-PC\Jeronimoh <jeronimoh974.88@gmail.com>
2017-12-11 14:50:41 +0400

    Mobs à distance get player collider

---
+++ Assets/MobGetCollider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobGetCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		GetComponent<ParticleSystem> ().trigger.SetCollider (0, GameManager.instanceGM.playerObj.GetComponent<MeshCollider>());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
